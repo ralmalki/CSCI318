458d97fb21ade6151cc63d8941e90a1ec2cf62bb
David <65207557+jungleOverload@users.noreply.github.com>
2020-09-23 14:50:25 +1000

    recurse

--- CSCI318_Group_Project/src/RecursiveDistance.java
+++ CSCI318_Group_Project/src/RecursiveDistance.java
@@ -6,42 +6,53 @@ import java.lang.reflect.Field;
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 /**
  *
- * @author David
- * For unequal references, a measure of the difference between
-the corresponding objects. This will be the same notion of
-object distance, applied recursively
-* 
-* 
-* Recursive distance: A measure entirely determined by the values
-of the distances between (recursively) objects p.r and q.r, for
-all matching reference attributes r such that p.r = q.r (both
-non-Void)
+ * @author David For unequal references, a measure of the difference between the
+ * corresponding objects. This will be the same notion of object distance,
+ * applied recursively
+ *
+ *
+ * Recursive distance: A measure entirely determined by the values of the
+ * distances between (recursively) objects p.r and q.r, for all matching
+ * reference attributes r such that p.r = q.r (both non-Void)
  */
 public class RecursiveDistance {
-    
-    //accepts one isngle object
-    public static <T> double distance(T one){   
+
+    //accepts one single object
+    public static <T> double distance(T one) {
         double distance = 0;
-        Field[] fields = one.getClass().getDeclaredFields();
+        Field[] allDeclaredFields = one.getClass().getDeclaredFields();
 
-        
-                for (int i = 0; i < fields.length; i++) {
-                    System.out.println(fields[i]); //debbug
-                    System.out.println(fields[i].getName()); //debug
-                    try {
-                        Class.forName(fields[i].getDeclaringClass().getName());
-                        System.out.println("Class found");
-                        //add to arraylist to check class object
-                    } catch (ClassNotFoundException e) {
-                        System.out.println("class not found");
-                    }
-                    System.out.println("-=-=-=-=");
+        //checks all fields for objects
+        for (int i = 0; i < allDeclaredFields.length; i++) {
+            //ignores all types that are not objects
+            if (!allDeclaredFields[i].getType().isPrimitive() && !allDeclaredFields[i].getType().isArray() && !allDeclaredFields[i].getType().getName().equals("java.lang.String")) {
+                
+                //do something with class name in here
+                String className = allDeclaredFields[i].getType().getName();
+                System.out.println("Class name: " + className);
+                
+            }
 
-                }
-                return distance;
+        }
+
+        /*
+        for (int i = 0; i < fields.length; i++) {
+            System.out.println(fields[i]); //debbug
+            System.out.println(fields[i].getName()); //debug
+            try {
+                Class.forName(fields[i].getDeclaringClass().getName());
+                System.out.println("Class found");
+                //add to arraylist to check class object
+            } catch (ClassNotFoundException e) {
+                System.out.println("class not found");
+            }
+            System.out.println("-=-=-=-=");
+
+        }
+*/
+        return distance;
     }
-    
+
 }
