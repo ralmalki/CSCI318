fd7170f02a78f2ea107d8771fcba07ecd07461bd
Riyad Almalki <eng.riyad@me.com>
2020-11-10 08:49:24 +1100

    Read JAR file and extract classes

--- CSCI318_Group_Project/src/MainScreen.java
+++ CSCI318_Group_Project/src/MainScreen.java
@@ -237,6 +237,10 @@ public class MainScreen extends javax.swing.JFrame {
         /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
          * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
          */
+        
+        String fileBath = "/Users/raymalk/Downloads/Calculator.jar";
+        AutoTest.openAndRestractJAR(fileBath);
+        
         try {
             for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
                 if ("Nimbus".equals(info.getName())) {
